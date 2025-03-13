de3b4a8c756481654236b0d1d505bbf16f63c626
Tianye Chen <tianyecf@gmail.com>
2025-03-13 17:19:04 -0400

    dotted background, revamped skills section

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,12 @@ import { gsap } from "gsap";
 import { ScrollTrigger } from "gsap/ScrollTrigger";
 import "./App.css";
 import { LuGraduationCap, LuMousePointer2, LuBriefcase } from "react-icons/lu";
-import { IoCodeSlash, IoMailOutline } from "react-icons/io5";
+import { IoCodeSlash, IoMailOutline, IoExtensionPuzzleOutline } from "react-icons/io5";
 import { FiGithub, FiLinkedin } from "react-icons/fi";
 import { PiHandWavingFill } from "react-icons/pi";
 import { IoIosArrowDown } from "react-icons/io";
 import { FaLaptopCode, FaRegUser } from "react-icons/fa";
+import { InfoBoxLarge } from "./Components/InfoBoxLarge";
 
 function App() {
   gsap.registerPlugin(ScrollTrigger);
@@ -33,14 +34,23 @@ function App() {
 
   const prog_skills = [
     "Python",
-    "JavaScript",
-    "C#",
-    "Java",
     "PyTorch",
-    "Unity",
+    "JavaScript",
     "React",
+    "C#",
+    "Unity",
+    "Java",
     "Firebase",
   ];
+
+  const hobbies = [
+    "Video Games",
+    "Model Building",
+    "Cooking",
+    "Gym",
+    "Exploring New Technologies",
+    "Visiting New Places",
+  ]
 
   const education = [
     {
@@ -74,6 +84,10 @@ function App() {
 
   return (
     <div class="min-h-screen" onMouseMove={handleMouseMove}>
+      <div
+        class="absolute min-h-screen min-w-screen bg-[radial-gradient(#e5e7eb_4px,transparent_0px)] [background-size:64px_64px]"
+      ></div>
+      
       <div class="relative flex min-h-screen flex-row items-center justify-center gap-8 overflow-hidden text-center md:text-left">
         <div class="">
           <h1 class="flex items-center text-3xl font-extrabold text-emerald-500 font-stretch-150%">
@@ -107,6 +121,45 @@ function App() {
         <IoIosArrowDown class="absolute bottom-10 flex justify-center text-4xl text-emerald-500" />
       </div>
 
+      <section class="bg-teal-50 pt-40 pb-80">
+        <div class="mx-auto max-w-4xl px-4">
+          <h2 class="mb-8 flex items-center justify-center text-3xl font-bold">
+            {" "}
+            <FaRegUser class="mr-2" /> About Me
+          </h2>
+          <p class="mb-8 text-lg font-light text-center">
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Pariatur, veniam minus eveniet ipsa excepturi, sit suscipit rerum quis commodi repellendus sapiente iusto incidunt corrupti voluptatum nobis officia magnam accusantium molestias?
+          </p>
+        </div>
+      </section>
+
+      <section class="pt-20 -translate-y-70">
+        <div class="mx-auto max-w-5/6 md:max-w-7xl border-1 border-gray-300 rounded-4xl shadow-gray-300 shadow-lg bg-white">
+          <div class='md:grid-cols-3 grid rounded-4xl'>
+            <InfoBoxLarge 
+              icon={<FaLaptopCode/>}
+              title='What I can do'
+              description={"Skills and areas of expertise I have acquired through my education and experience."}
+              content={broad_skills}
+              corner={'left'}
+            />
+            <InfoBoxLarge 
+              icon={<IoCodeSlash/>}
+              title='What I know'
+              description={"Programming languages and tools I am proficient in."}
+              content={prog_skills}
+            />
+            <InfoBoxLarge 
+              icon={<IoExtensionPuzzleOutline/>}
+              title='What my hobbies are'
+              description={"Interests and activities I enjoy outside of work."}
+              content={hobbies}
+              corner={'right'}
+            />
+          </div>
+        </div>
+      </section>
+      
       <section class="bg-teal-50 py-20">
         <div class="mx-auto max-w-4xl px-4">
           <h2 class="mb-8 flex items-center text-3xl font-bold">
@@ -136,47 +189,9 @@ function App() {
         <div class="mx-auto max-w-4xl px-4">
           <h2 class="mb-8 flex items-center text-3xl font-bold">
             {" "}
-            <IoCodeSlash class="mr-2" /> Technologies{" "}
-          </h2>
-          <div class="grid grid-cols-2 gap-4 md:grid-cols-4">
-            {prog_skills.map((skill, index) => (
-              <div
-                class="rounded-lg bg-teal-50 py-4 pr-32 pl-4 text-left font-light text-nowrap shadow-md"
-                key={index}
-              >
-                {skill}
-              </div>
-            ))}
-          </div>
-        </div>
-      </section>
-
-      <section class="bg-teal-50 py-20">
-        <div class="mx-auto max-w-4xl px-4">
-          <h2 class="mb-8 flex items-center text-3xl font-bold">
-            {" "}
-            <LuMousePointer2 class="mr-2" /> Skills{" "}
-          </h2>
-          <div class="grid-col-2 grid gap-4 md:grid-cols-4">
-            {broad_skills.map((skill, index) => (
-              <div
-                class="rounded-lg bg-white py-4 pr-24 pl-4 text-left font-light text-nowrap shadow-md"
-                key={index}
-              >
-                {skill}
-              </div>
-            ))}
-          </div>
-        </div>
-      </section>
-
-      <section class="py-20">
-        <div class="mx-auto max-w-4xl px-4">
-          <h2 class="mb-8 flex items-center text-3xl font-bold">
-            {" "}
             <LuBriefcase class="mr-2" /> Experience{" "}
           </h2>
-          <div class="grid-cols1 grid gap-12">
+          <div class="grid-cols-1 grid gap-12">
             {experience.map((exp, index) => (
               <div class="border-l-2 border-emerald-200 pl-8">
                 <p class="text-lg font-semibold">
@@ -198,15 +213,6 @@ function App() {
           <h2 class="mb-8 flex items-center text-3xl font-bold">
             {" "}
             <FaLaptopCode class="mr-2" /> Projects{" "}
-          </h2>
-        </div>
-      </section>
-
-      <section class="py-20">
-        <div class="mx-auto max-w-4xl px-4">
-          <h2 class="mb-8 flex items-center text-3xl font-bold">
-            {" "}
-            <FaRegUser class="mr-2" /> About Me
           </h2>
         </div>
       </section>
---
+++ src/Components/InfoBoxLarge.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+
+export const InfoBoxLarge = ({icon, title, description, content, corner}) => {
+    return (
+        <div class={`border-1 border-gray-200 px-4 py-16 hover:scale-110 transition-all ease-in-out bg-white hover:rounded-4xl hover:shadow-lg
+        ${corner === 'left' ? 'rounded-l-4xl' : ''} ${corner === 'right' ? 'rounded-r-4xl' : ''}`}>
+            <div class='flex items-center justify-center flex-col text-center'>
+                <div class='text-4xl mb-8'>
+                    {icon}
+                </div>
+                <div class='mb-8'>
+                    <h2 class='text-2xl font-bold mb-4'>{title}</h2>
+                    <p class='text-gray-500 text-center'>{description}</p>
+                </div>
+                {content.map((item, index) => (
+                    <div>
+                        <p class='text-lg font-semibold py-2'>{item}</p>
+                    </div>
+                ))}
+            </div>
+        </div>
+    );   
+}
