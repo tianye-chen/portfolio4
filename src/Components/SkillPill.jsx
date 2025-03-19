b40bdba3df077d3e6f3af8b05f34ab4463e0d7d2
Tianye Chen <tianyecf@gmail.com>
2025-03-19 18:24:45 -0400

    hover effect on pillbox

--- src/Components/SkillPill.jsx
+++ src/Components/SkillPill.jsx
@@ -1,9 +1,9 @@
-import React from 'react'
+import React from "react";
 
-export const SkillPill = ({skill}) => {
-    return (
-        <div class="flex justify-center bg-emerald-500 overflow-hidden px-2 rounded-full whitespace-nowrap text-white">
-            {skill}
-        </div>
-    )
-}
+export const SkillPill = ({ skill }) => {
+  return (
+    <div class="flex justify-center overflow-hidden rounded-full bg-emerald-500 px-2 pb-0.5 whitespace-nowrap text-white outline-emerald-500 transition-all ease-in-out hover:bg-white hover:text-emerald-500 hover:outline-1">
+      {skill}
+    </div>
+  );
+};
