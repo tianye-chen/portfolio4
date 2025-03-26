a0123ad7d815a9eb1350a267427a03b07a4dd5a8
Tianye Chen <tianyecf@gmail.com>
2025-03-26 16:26:26 -0400

    improve edu responsiveness and add school icons

--- src/Components/EducationBox.jsx
+++ src/Components/EducationBox.jsx
@@ -1,4 +1,4 @@
-import React, { useRef } from "react";
+import React, { use, useEffect, useRef } from "react";
 import { gsap } from "gsap";
 import { useGSAP } from "@gsap/react";
 import { Flip, TextPlugin } from "gsap/all";
@@ -23,6 +23,22 @@ export const EducationBox = ({
   const corners = idx === 0 ? "rounded-l-4xl" : "rounded-r-4xl";
   const animTransition = "transition-all ease-in-out";
   const eduButtonTextRef = useRef();
+  const sidePanelRef = useRef();
+  const course_list = (
+    <div class="relative flex max-w-fit flex-col items-center border-y py-4 text-nowrap xl:ml-6 xl:items-start xl:py-12">
+      {courses.map((course, index) => (
+        <div class="transition-transform duration-200 hover:scale-110">
+          <p class="mr-2 text-xl">{course}</p>
+        </div>
+      ))}
+    </div>
+  );
+
+  useGSAP(() => {
+    return () => {
+      gsap.killTweensOf(eduButtonTextRef.current);
+    };
+  }, []);
 
   const handleButtonClick = () => {
     handleFocus();
@@ -45,7 +61,7 @@ export const EducationBox = ({
 
   return (
     <div
-      class={`group/main relative z-10 min-h-[60vh] bg-center text-white ${corners} ${visible ? "min-w-1/2" : ""} ${selected ? "min-w-screen" : "min-w-0"} ${animTransition} duration-1000`}
+      class={`group/main relative z-10 flex min-h-[100vh] items-center justify-center bg-center text-white xl:block xl:min-h-[45rem] ${corners} ${visible ? "min-w-1/2" : ""} ${selected ? "min-w-screen" : "min-w-0"} ${animTransition} duration-1000`}
       style={{ backgroundImage: `url(${bg}.jpg)`, backgroundSize: "auto" }}
     >
       <div
@@ -53,36 +69,48 @@ export const EducationBox = ({
       ></div>
 
       <div
-        class={`absolute z-10 flex min-h-full min-w-1/2 flex-col justify-center ${selected ? "right-0" : "invisible -right-[75vw]"} ${animTransition} duration-1000`}
+        class={`absolute z-10 hidden min-h-full min-w-1/2 flex-col justify-center xl:flex ${selected ? "right-0" : "invisible -right-[75vw]"} ${animTransition} duration-1000`}
+        ref={sidePanelRef}
       >
-        {/*TODO: Make absolutes relative to the relevant courses box*/}
-        <div class="ml-6 flex max-w-6/9 flex-col items-start justify-start border-y py-12 text-nowrap">
-          {courses.map((course, index) => (
-            <div class={`hover:scale-110 ${animTransition} duration-200`}>
-              <p class="mr-2 text-3xl">{course}</p>
-            </div>
-          ))}
-        </div>
+        {course_list}
 
-        <div class="font-lexend mb-4 text-9xl tracking-widest text-nowrap">
-          <h1 class="absolute top-0">RELEVANT</h1>
-          <h1 class="absolute top-[20rem] -right-[17rem] rotate-90">COURSES</h1>
-          <h1 class="absolute bottom-0">CGPA: {gpa}</h1>
+        <div class="font-lexend mb-4 hidden tracking-widest text-nowrap xl:block xl:text-8xl 2xl:absolute 2xl:text-9xl">
+          <h1 class="absolute top-0 2xl:-top-[23rem]">RELEVANT</h1>
+          <h1 class="absolute top-[21rem] left-[23rem] rotate-90 2xl:-top-[3rem] 2xl:left-[31rem]">
+            COURSES
+          </h1>
+          <h1 class="absolute bottom-0 2xl:-bottom-[23rem]">CGPA: {gpa}</h1>
         </div>
       </div>
 
-      <div class="relative h-full min-h-[60vh] w-[50vw]">
+      <div class="relative h-full min-h-[100vh] w-[100vw] md:w-[50vw] lg:min-h-[60vh]">
         <div
-          class={`absolute flex min-h-full min-w-full flex-col justify-center text-center ${animTransition} text-nowrap duration-[850ms]`}
+          class={`absolute flex min-h-full min-w-full flex-col items-center justify-center px-10 text-center ${animTransition} 3xl:text-nowrap duration-[850ms]`}
         >
-          <p class="font-lexend text-6xl font-semibold">{university}</p>
-          <p class="text-sm">{duration}</p>
-          <p class="mb-2 font-semibold">{degree}</p>
-          <p class="">Focus in {focusArea}</p>
+          <div class="relative mb-2">
+            <img src={`${bg}_icon.png`} class="max-w-48" />
+            <img
+              src={`${bg}_icon.png`}
+              class="absolute top-1 left-1 -z-10 max-w-48 brightness-30"
+            />
+          </div>
+
+          <div className="mb-12">
+            <p class="font-lexend mb-4 text-6xl font-semibold">{university}</p>
+            <p class="text-sm">{duration}</p>
+            <p class="mb-2 font-semibold">{degree}</p>
+            <p class="xl:mb-6">Focus in {focusArea}</p>
+          </div>
+
+          <div class="xl:hidden">
+            <p class="mb-2 text-xl font-bold">Relevant Courses</p>
+            <div class="mb-2">{course_list}</div>
+            <p class="text-xl font-bold">CGPA: {gpa}</p>
+          </div>
         </div>
 
         <div
-          class={`group/button absolute bottom-1/5 left-1/2 hidden min-w-xs -translate-1/2 items-center justify-center overflow-hidden text-nowrap xl:flex ${animTransition} duration-[850ms]`}
+          class={`group/button absolute bottom-1/6 left-1/2 hidden min-w-xs -translate-1/2 items-center justify-center overflow-hidden text-nowrap xl:flex ${animTransition} duration-[850ms]`}
           onClick={handleButtonClick}
         >
           <div
