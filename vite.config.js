ad470e4dea4f12859325e0f089a9f7943ca37446
Tianye Chen <tianyecf@gmail.com>
2025-04-12 17:49:53 -0400

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: 'https://tianye-chen.github.io/portfolio4/',
+  base: '/',
   plugins: [
     react(), 
     tailwindcss()
