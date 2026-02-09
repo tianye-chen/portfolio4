03c83eeef35025d3d1627d63e2fb2b9b993f683c
Tianye Chen <tianyecf@gmail.com>
2026-02-09 18:35:10 -0500

    add redirects for netlify.toml

--- client/src/Data/data.jsx
+++ client/src/Data/data.jsx
@@ -175,6 +175,7 @@ export const projects = [
     year: "2026",
     attributes: ["Solo"],
     repo: "https://github.com/tianye-chen/portfolio4/tree/main/client/src/Pages/Blog",
+    demo: "https://tianye-chen.github.io/portfolio4/blog",
     tech: [
       "JavaScript",
       "React",
